a8c6a88dfad0bb64df974da7157bc4eeb4073d84
aaliyanamingle39@gmail.com <aaliyanamingle39@gmail.com>
2024-11-30 17:22:59 +0000

    config files for helm charts

--- terraform/provider.tf
+++ terraform/provider.tf
@@ -10,3 +10,19 @@ terraform {
 provider "aws" {
   
 }
+
+# data "aws_eks_cluster" "cluster" {
+#   name = module.eks.cluster_name
+# }
+
+# provider "helm" {
+#   kubernetes {
+#     host                   = module.eks.Cluster_endpoint
+#     cluster_ca_certificate = we need the cluster certificate 
+#     exec {
+#       api_version = "client.authentication.k8s.io/v1beta1"
+#       args        = ["eks", "get-token", "--cluster-name", data.aws_eks_cluster.cluster.name]
+#       command     = "aws"
+#     }
+#   }
+# }
